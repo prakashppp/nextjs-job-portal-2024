8a82b98eb072be71d577a6e1b86b9d55f96f4b4b
Prakash Jagtap <prakashjagtap738760@gmail.com>
2024-07-29 15:50:08 +0530

    remove unwanted

--- src/app/page.js
+++ src/app/page.js
@@ -14,7 +14,7 @@ async function Home() {
   return (
     <Fragment>
       <section className="relative w-full h-full min-h-screen pb-10">
-        <div className="w-full h-full relative">
+        {/* <div className="w-full h-full relative"> */}
           <div className="flex flex-col-reverse lg:flex-row gap-10 mt-16">
             <section className="w-full lg:w-[50%] flex flex-col md:px-2 lg:px-0 p-5 lg:p-10">
               <div className="w-full flex justify-start flex-col h-auto lg:pt-7">
@@ -43,7 +43,7 @@ async function Home() {
               />
             </section>
           </div>
-        </div>
+        {/* </div> */}
       </section>
     </Fragment>
   );
