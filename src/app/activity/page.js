52ecdd7dff6cafbf4e699fb7f37b56f862907d50
Prakash Jagtap <prakashjagtap738760@gmail.com>
2024-07-30 17:45:41 +0530

    bug solve

--- src/app/activity/page.js
+++ src/app/activity/page.js
@@ -1,12 +1,18 @@
 import {
   fetchJobApplicationsForCandidate,
   fetchJobsForCandidateAction,
+  fetchProfileAction,
 } from "@/actions";
 import CandidateActivity from "@/components/candidate-activity";
 import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
 export default async function Activity() {
   const user = await currentUser();
+  const profileInfo = await fetchProfileAction(user?.id);
+   
+  if(profileInfo?.role === 'recruiter') redirect('/')
+
   const jobList = await fetchJobsForCandidateAction();
   const jobApplicants = await fetchJobApplicationsForCandidate(user?.id);
 
